202a4c5644458a9f356ec215d1fcdd46e2bd90ed
Sinkira <sinkirayt@gmail.com>
2023-08-13 20:36:51 +0400

    probably google analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script>
 	import Navbar from "./Navbar.svelte";
+
+	try {
+        if(typeof window !== 'undefined' 
+        && window) {
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RDYGGB43F6');
+        }
+    } catch(error) {
+		console.error("Failed to initialize Analytics")
+	}
 </script>
+
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-RDYGGB43F6"></script>
+</svelte:head>
+
 
 <Navbar />
 
